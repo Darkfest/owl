2441d857e28fd502e0c130e73f65024e4a4bfb70
Darkfest <mytinymagic@gmail.com>
2019-04-20 00:30:54 +0600

    Gulp-file: comments added, minor rework.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,79 @@
-const 	gulp 			= require("gulp"),
-		pug 			= require("gulp-pug"),
-		browserSync 	= require("browser-sync").create(),
-		sass			= require("gulp-sass"),
-		autoprefixer	= require("gulp-autoprefixer"),
-		babel			= require("gulp-babel");
+/* Used plugins */
+const 	gulp = require("gulp"),
+		pug = require("gulp-pug"),
+		browserSync = require("browser-sync").create(),
+		sass = require("gulp-sass"),
+		autoprefixer = require("gulp-autoprefixer"),
+		babel = require("gulp-babel");
 
-gulp.task("html", (done) => {
-	gulp.src("./app/pug/index.pug")
-	.pipe(pug({pretty: true}).on("error", (e)=>{console.log(e.toString());}))
-	.pipe(gulp.dest("./dist/"));
-	done();
-});
+/* Error handler-function */
+function errorHandler(error) {
+	console.log(error.toString());
+}
 
-gulp.task("css", (done) => {
-	gulp.src("./app/scss/mainstyle.scss")
-	.pipe(sass().on("error", (e)=>{console.log(e.toString());}))
+/* HTML handler-function:
+** one-source-file >
+** pug-to-HTML + log-errors >
+** one-output-file >
+** live-reload-after-compiling.
+*/
+function htmlHandler() {
+	return gulp.src("./app/pug/index.pug")
+	.pipe(pug({pretty: true}).on("error", errorHandler))
+	.pipe(gulp.dest("./dist/"))
+	.pipe(browserSync.stream());
+}
+
+/* CSS handler-function:
+** one-source-file(scss) >
+** SASS-to-CSS + log-errors >
+** add-prefixes >
+** one-output-file >
+** inject-changes-into-browser.
+*/
+function cssHandler() {
+	return gulp.src("./app/scss/mainstyle.scss")
+	.pipe(sass().on("error", errorHandler))
 	.pipe(autoprefixer({
 		browsers: ["last 3 versions"],
 		cascase: false
 	}))
-	.pipe(gulp.dest("./dist/css/"));
-	done();
-});
+	.pipe(gulp.dest("./dist/css/"))
+	.pipe(browserSync.stream());
+}
 
-gulp.task("js", (done) => {
-	gulp.src("./app/js/mainscript.js")
-	.pipe(babel().on("error", (e)=>{console.log(e.toString());}))
-	.pipe(gulp.dest("./dist/js/"));
-	done();
-});
+/* JavaScript hadler-function:
+** one-source-file >
+** ES6-to-ES6 + log-errors >
+** one-output-file >
+** live-reload-after-compiling.
+*/
+function jsHandler() {
+	return gulp.src("./app/js/mainscript.js")
+	.pipe(babel().on("error", errorHandler))
+	.pipe(gulp.dest("./dist/js/"))
+	.pipe(browserSync.stream());
+}
 
-gulp.task("watch", (done)=>{
+/* Start Server function */
+function serverInit() {
 	browserSync.init({
-		server: "./dist"
+		server: {
+			baseDir: "./dist"
+		}
 	});
-	gulp.watch("./app/scss/*.scss", gulp.parallel("css"));
-	gulp.watch("./app/pug/*.pug", gulp.parallel("html"));
-	gulp.watch("./app/js/*.js", gulp.parallel("js"));
-	gulp.watch("./dist/**/*.*").on("change", browserSync.reload);
-	done();
-});
+}
 
-gulp.task("default", gulp.parallel("watch"));
+/* Watch function:
+** start-server +
+** watch-files.
+*/
+function watch() {
+	serverInit();
+	gulp.watch("./app/scss/*.scss", cssHandler);
+	gulp.watch("./app/pug/*.pug", htmlHandler);
+	gulp.watch("./app/js/*.js", jsHandler);
+}
+
+/* Default task is: watch */
+gulp.task("default", watch);
